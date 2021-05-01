57aad29b240638bdc15bc995a946abfc857237ee
Junette Lee <jlee63@gmail.com>
2021-05-01 16:34:38 -0500

    Data from SQLite to JSON formatting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-# import necessary libraries
+# Import libraries
 import os
+import collections
 from flask import (
     Flask,
     render_template,
@@ -17,90 +18,66 @@ app = Flask(__name__)
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
-## rename olympicsdb 
+
+# Define database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///olympics.sqlite"
 
 # Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-# Columns already defined? 
+
+# Define database table and column attributes
 class Olympics(db.Model):
-        __tablename__ = 'olympics'
+    __tablename__ = 'olympics'
 
-        primary_id = db.Column(db.Integer, primary_key=True)
-        NOC = db.Column(db.String(10))
-        Region = db.Column(db.String(100))
-        Medal = db.Column(db.String(20))
-        Year = db.Column(db.Integer)
-        def __repr__(self):
-            return '<Olympics %r>' % (self.name)
+    primary_id = db.Column(db.Integer, primary_key=True)
+    NOC = db.Column(db.String(10))
+    Region = db.Column(db.String(100))
+    Medal = db.Column(db.String(20))
+    Year = db.Column(db.Integer)
 
-# create route that renders index.html template
+    def __repr__(self):
+        return '<Olympics %r>' % (self.name)
+
+# Flask route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-# Remove??
-# @app.route("/send", methods=["GET", "POST"])
-# def send():
-#     if request.method == "POST":
-#         name = request.form["petName"]
-#         lat = request.form["petLat"]
-#         lon = request.form["petLon"]
-
-#         pet = Pet(name=name, lat=lat, lon=lon)
-#         db.session.add(pet)
-#         db.session.commit()
-#         return redirect("/", code=302)
-
-#     return render_template("form.html")
-
-#Olympics.all columns - 
+# Flask route to query data from SQLite database and output in json format
 @app.route("/api/olympics")
 def olympics():
     results = db.session.query(Olympics.primary_id, Olympics.NOC, Olympics.Region, Olympics.Medal, Olympics.Year).all()
 
-    primary_id = [result[0] for result in results]
-    NOC = [result[1] for result in results]
-    Region = [result[2] for result in results]
-    Medal = [result[3] for result in results]
-    Year = [result[4] for result in results]
+    olympics_data = []
 
-    olympics_data = {
-        "primary_id": {
-            "NOC": [],
-            "Region": [],
-            "Medal": [],
-            "Year": [],
-        }
-    }
-   for i in len(primary_id):
-       olympics_data["primary_id"].append(primary_id[i])
-       olympics_data["NOC"].append(NOC[i])
-       olympics_data["Region"].append(Region[i])
-       olympics_data["Medal"].append(Medal[i])
-       olympics_data["Year"].append(Year[i])
+    # Loop through query results and assign key value pairs for each row
+    for result in results:
+        dict = collections.OrderedDict()
+        dict["primary_id"] = result[0]
+        dict["NOC"] = result[1]
+        dict["Region"] = result[2]
+        dict["Medal"] = result[3]
+        dict["Year"] = result[4]
+        olympics_data.append(dict)
+
+    # Loop through query results and assign variables to each series
+    # primary_id = [result[0] for result in results]
+    # NOC = [result[1] for result in results]
+    # Region = [result[2] for result in results]
+    # Medal = [result[3] for result in results]
+    # Year = [result[4] for result in results]
+
+    # Assign key value pairs to data series
     # olympics_data = [{
-    #     #"type": "scattergeo",
-    #     #"locationmode": "USA-states",
     #     "primary_id": primary_id,
     #     "NOC": NOC,
     #     "Region": Region,
     #     "Medal": Medal,
-    #     "Year": Year,
-    #     #"text": hover_text,
-    #     #"hoverinfo": "text",
-    #     #"marker": {
-    #         #"size": 50,
-    #         #"line": {
-    #             #"color": "rgb(8,8,8)",
-    #             #"width": 1
-    #         #},
-    #     #}
+    #     "Year": Year
     # }]
-
+    
     return jsonify(olympics_data)
 
 

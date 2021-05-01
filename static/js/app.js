e5b79024c8433f50d7deb79f86bcd8e35bcd0269
Junette Lee <jlee63@gmail.com>
2021-05-01 17:27:14 -0500

    Framework data flow

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,25 @@
-// Data file paths ** REPLACE WITH DATABASE PATH LATER **
-// const athleteURL = "../../data/athlete_events.csv";
-// const nocURL = "../../data/noc_regions.csv";
-// // Test data read
-// var athleteData = d3.csv(athleteURL);
-// athleteData.then(row => {
-//   console.log("Athlete Data:", row);
+// Data file path
+const url = "/api/olympics";
+
+// Assign d3.json to variable
+var olympicsData = d3.json(url);
+
+// Test data read
+olympicsData.then(function(response) {
+  console.log("Olympics Data:", response);
+});
+
+// olympicsData.then(row => {
+//   console.log("Olympics Data:", row);
 // });
-// var nocData = d3.csv(nocURL)
-// nocData.then(row => {
-//   console.log("NOC Region Data:", row);
-// });
+
 // Create reference variables
 const dropdownElement = d3.select("#selCountry");
 const nocElement = d3.select("#noc");
-function init() {
-    const url = "/api/olympics";
-    d3.json(url).then(function(response) {
-      console.log(response);
-      const data = response;
 
-    })
+
+function init() {
+
 //   // Add National Olympic Committees to dropdown
 //   nocData.then(row => {row.region
 //     .forEach(code => dropdownElement
@@ -28,17 +28,19 @@ function init() {
 //       .property("value")
 //     );
 //   });
-  // // Default plot
-  // athleteData.then(row => {
-  //   buildDashboard(row.NOC[0])
-  //   console.log("Default NOC:", row.NOC[0])
-  // });
-  // // Default plots for selected data
-  // athleteData.then(row => {
-  //   buildDashboard(row.NOC[0])
-  //   console.log("Default NOC:", row.NOC[0])
-  // });
-// };
+//   // Default plot
+//   athleteData.then(row => {
+//     buildDashboard(row.NOC[0])
+//     console.log("Default NOC:", row.NOC[0])
+//   });
+//   // Default plots for selected data
+//   athleteData.then(row => {
+//     buildDashboard(row.NOC[0])
+//     console.log("Default NOC:", row.NOC[0])
+//   });
+};
+
+
 // // Event handler for new selection
 // function optionChanged(nocSelection) {
 //   console.log(`National Olympic Committee: ${nocSelection}`);
@@ -63,38 +65,30 @@ function init() {
 //     });
 //     console.log("parsed int data", data);
 //     // var barDashTrace = {
-    //   x: selectionData.noc.reverse(),
-    //   y: selectionData.noc.reverse(),
-    // }
-    // var barLayout = {
-    //   scope: "usa",
-    //   title: "Pet Pals",
-    //   showlegend: false,
-    //   height: 600,
-            // width: 980,
-      // geo: {
-      //   scope: "usa",
-      //   projection: {
-      //     type: "albers usa"
-      //   },
-      //   showland: true,
-      //   landcolor: "rgb(217, 217, 217)",
-      //   subunitwidth: 1,
-      //   countrywidth: 1,
-      //   subunitcolor: "rgb(255,255,255)",
-      //   countrycolor: "rgb(255,255,255)"
-    // };
+//       x: selectionData.noc.reverse(),
+//       y: selectionData.noc.reverse(),
+//     }
+//     var barLayout = {
+//       scope: "usa",
+//       title: "Pet Pals",
+//       showlegend: false,
+//       height: 600,
+//             width: 980,
+//       geo: {
+//         scope: "usa",
+//         projection: {
+//           type: "albers usa"
+//         },
+//         showland: true,
+//         landcolor: "rgb(217, 217, 217)",
+//         subunitwidth: 1,
+//         countrywidth: 1,
+//         subunitcolor: "rgb(255,255,255)",
+//         countrycolor: "rgb(255,255,255)"
+//     };
 //   });
 //     // Plotly.newPlot("plot", data, layout);
 // };
+
 init();
-
-
-
-
-
-
-
-
-
 
